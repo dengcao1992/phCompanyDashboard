728e585f554278dac8fa10b466caef41a3f21d1a
dcs <419787760@qq.com>
2018-10-19 05:38:41 -0400

    nation销售份额 卡片数据

---
+++ app/services/FindNationSaleShareByCompanyMarketAndYM.scala
@@ -0,0 +1,42 @@
+package services
+
+import com.pharbers.builder.dashboard.phNationDashboard
+import com.pharbers.jsonapi.json.circe.CirceJsonapiSupport
+import com.pharbers.jsonapi.model
+import play.api.mvc.Request
+import com.pharbers.macros._
+import com.pharbers.macros.convert.jsonapi.JsonapiMacro._
+import com.pharbers.search.phMaxNativeDashboard
+import com.pharbers.driver.util.PhRedisTrait
+import com.pharbers.dbManagerTrait.dbInstanceManager
+import com.pharbers.models.entity.max._
+import com.pharbers.models.request.request
+
+case class FindNationSaleShareByCompanyMarketAndYM()(implicit val rq: Request[model.RootObject], dbt: dbInstanceManager)
+        extends phNationDashboard with CirceJsonapiSupport with RequestHand {
+
+    def selectNationSaleShare():  model.RootObject ={
+        val nationSaleShare = new NationSaleShare()
+        requestData = formJsonapi[request](rq.body)
+        init()
+        val ym = time.replaceAll("-", "")
+        val dashboard = phMaxNativeDashboard(companyId, ym, market)
+        nationSaleShare.SaleShareCard = Some(findSaleShareCardList(dashboard))
+        toJsonapi(nationSaleShare)
+    }
+
+    private def findSaleShareCardList(dashboard: phMaxNativeDashboard): List[SaleShareCard] ={
+        List(
+            new SaleShareCard("市场总销售额", time, "全国", getFormatSales(dashboard.getCurrMonthNationSales),
+                "mil", getFormatShare(dashboard.getNationSalesYearOnYear), getFormatShare(dashboard.getNationSalesMonthOnMonth)
+            ),
+            new SaleShareCard("产品销售额", time, "全国", getFormatSales(dashboard.getCurrMonthCompanySales), "mil",
+                getFormatShare(dashboard.getCompanySalesYearOnYear), getFormatShare(dashboard.getCompanySalesMonthOnMonth)
+            ),
+            new SaleShareCard("产品份额", time, "全国", getFormatShare(dashboard.getCurrMonthCompanyShare), "%",
+                getFormatShare(dashboard.getCompanyShareYearOnYear), getFormatShare(dashboard.getCompanyShareMonthOnMonth)
+            )
+        )
+    }
+
+}
