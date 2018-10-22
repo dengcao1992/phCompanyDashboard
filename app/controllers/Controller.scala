90adca5dbae1511542df86afd83cd2de1c87c89c
dcs <419787760@qq.com>
2018-10-21 23:02:49 -0400

    Merge branch 'dcs-1018' of https://github.com/dengcao1992/phCompanyDashboard into dcs-1018
    Conflicts:
    	app/services/FindSalesOverviewByCompanyAndYM.scala

--- app/controllers/Controller.scala
+++ app/controllers/Controller.scala
@@ -17,7 +17,7 @@ class Controller @Inject()(implicit val cc: ControllerComponents,
                            implicit val dbt: dbInstanceManager)
         extends AbstractController(cc) with Circe with CirceJsonapiSupport {
 
-    def routes(pkg: String, pkg2: String): Action[RootObject] = Action(circe.json[RootObject]) {
+    def routes(pkg: String, pkg2: String): Action[RootObject] = Action(circe.json[RootObject]) { //捕获异常
         implicit request =>
         Ok(
             (pkg, pkg2) match {
@@ -35,7 +35,7 @@ class Controller @Inject()(implicit val cc: ControllerComponents,
                 (pkg1, pkg2) match{
                     case ("nation", "saleShare") => FindNationSaleShare().selectNationSaleShare().asJson
                     case ("nation", "marketTrend") => FindNationMarketTrend().selectNationMarketTrend().asJson
-                    case ("nation", "saleShare") => FindNationMostWord().selectNationMostWord().asJson
+                    case ("nation", "mostWord") => FindNationMostWord().selectNationMostWord().asJson
                 }
             )
     }
