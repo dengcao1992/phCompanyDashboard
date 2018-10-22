d04dd0505ea4ff6dbcbbfe7ebdf05c1545c68bb1
dcs <419787760@qq.com>
2018-10-22 08:21:06 -0400

    Merge branch 'dcs-1018' of https://github.com/dengcao1992/phCompanyDashboard into dcs-1018
    Conflicts:
    	app/controllers/Controller.scala
    	app/services/FindCompanyProdSalesContribution.scala

--- app/services/FindProvinceLineOverview.scala
+++ app/services/FindProvinceLineOverview.scala
@@ -19,13 +19,13 @@ case class FindProvinceLineOverview()(implicit val rq: Request[model.RootObject]
         requestData = formJsonapi[request](rq.body)
         init()
         val dashboard = phMaxProvinceDashboard(companyId, ym, market)
-        provinceLineOverview.MixedGraphData = Some(findMixedGraphDataList(dashboard))
+        provinceLineOverview.MixedGraphLine = Some(findMixedGraphDataList(dashboard))
         toJsonapi(provinceLineOverview)
     }
 
-    private def findMixedGraphDataList(dashboard: phMaxProvinceDashboard): List[MixedGraphData] = {
+    private def findMixedGraphDataList(dashboard: phMaxProvinceDashboard): List[MixedGraphLine] = {
         dashboard.getProvinceSalesLstMap(ym).map(m => {
-            val mixedGraphData = new MixedGraphData()
+            val mixedGraphData = new MixedGraphLine()
             mixedGraphData.province = m.getOrElse("province", "无")
             mixedGraphData.scale = getFormatSales(m.getOrElse("provinceSales", "0.0").toDouble)
             mixedGraphData.market_growth = getFormatShare(m.getOrElse("provMomGrowth", "0.0").toDouble)
